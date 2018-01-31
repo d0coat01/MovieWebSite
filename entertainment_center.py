d3fd7be5b7567fe162a2c013fba5e09d61dcde49
Daniel Coats <d0coat01@louisville.edu>
2018-01-30 20:26:24 -0800

    Movie class created in media.py. List of Movie objects and call to fresh_tomatoes.py made in entertainment_center.py.
    Signed-off-by: Daniel Coats <d0coat01@louisville.edu>

---
+++ entertainment_center.py
@@ -0,0 +1,23 @@
+# media contains the Movie class
+import media
+# fresh_tomatoes generates a html page from a list of Movie objects.
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "pictures/toy_story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+predator = media.Movie("Predator",
+                       "pictures/predator.jpg",
+                       "https://www.youtube.com/watch?v=DrCqEA4r_R4")
+
+conan = media.Movie("Conan the Barbarian",
+                    "pictures/conan.jpg",
+                    "https://www.youtube.com/watch?v=B3YwzRmBPLY")
+
+lotr_two_towers = media.Movie("Lord of the Rings: The Two Towers",
+                              "pictures/lotr_two_towers.jpg",
+                              "https://www.youtube.com/watch?v=gXC-jJhFaUI")
+
+movies = [toy_story, predator, conan, lotr_two_towers]
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,14 @@
+class Video:
+
+    def __init__(self, title):
+        self.title = title
+
+
+class Movie(Video):
+
+    """Movie class initializes with a title, image url, and trailer url"""
+
+    def __init__(self, movie_title, movie_image_url,  movie_trailer_url):
+        Video.__init__(self, movie_title)
+        self.trailer_youtube_url = movie_trailer_url
+        self.poster_image_url = movie_image_url
